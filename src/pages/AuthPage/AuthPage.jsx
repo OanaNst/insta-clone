fbbbc9f2ed44ba6de21c33fb999cf5a1cb22e4fc
Oana Niste <oananiste@gmail.com>
2023-12-12 11:38:13 +0100

    Fix content center issue in small displays

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -6,7 +6,7 @@ const AuthPage = () => {
     <>
       <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
         <Container maxW={"container.md"} padding={0}>
-          <Flex>
+          <Flex justifyContent={"center"}>
             {/* Left side content */}
             <Box display={{base:"none", md:"block"}}>
               <Image src='/auth.png' h={650} alt='Phone image'/>
