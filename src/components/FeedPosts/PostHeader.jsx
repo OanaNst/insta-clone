5dc221211d12afc232163006fbb7da81c339b3b9
Oana Niste <oananiste@gmail.com>
2024-01-02 16:46:39 +0100

    Add username, avatar and img props

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,13 +1,13 @@
 import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
 
-const PostHeader = () => {
+const PostHeader = ({username, avatar}) => {
   return (
     <>
       <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
         <Flex alignItems={"center"} gap={2}>
-          <Avatar src="/img1.png" alt="User profile pic" size={"sm"}/>
+          <Avatar src={avatar} alt="User profile pic" size={"sm"}/>
           <Flex fontSize={12} fontWeight={"bold"} gap={2}>
-            OanaNst
+            {username}
             <Box color={"gray.500"}>
               1 w
             </Box>
