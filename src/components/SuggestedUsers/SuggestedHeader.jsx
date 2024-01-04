78bfa81365587d1ac3308eef76c00c035d560ea0
Oana Niste <oananiste@gmail.com>
2024-01-04 09:59:58 +0100

    Create suggested header structure

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,8 +1,18 @@
+import { Avatar, Text, Flex, Link } from "@chakra-ui/react"
+import {Link as RouterLink} from "react-router-dom";
 
 
 const SuggestedHeader = () => {
   return (
-    <div>SuggestedHeader</div>
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar name="OanaNst" size={"sm"} src="/profilepic.png"/>
+        <Text fontSize={12} fontWeight={"bold"}>OanaNst</Text>
+      </Flex>
+      <Link as={RouterLink} to={"/auth"} fontSize={14} fontWeight={"medium"} color={"blue.400"} cursor={"pointer"} style={{textDecoration:"none"}}>
+        Log Out
+      </Link>
+    </Flex>
   )
 }
 
