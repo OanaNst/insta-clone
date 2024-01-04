475ec0c0d2ecbee9e1402c75a6573e88b3c7ade9
Oana Niste <oananiste@gmail.com>
2024-01-04 15:07:39 +0100

    Add profile page structure

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,21 @@
+import { Container, Flex } from "@chakra-ui/react"
+import ProfileHeader from "../../components/Profile/ProfileHeader"
+import ProfileTabs from "../../components/Profile/ProfileTabs"
+import ProfilePosts from "../../components/Profile/ProfilePosts"
 
 const ProfilePage = () => {
   return (
-    <div>ProfilePage</div>
+    <Container maxW={"container.large"} py={5}>
+      {/* Flex container for the profile header */}
+      <Flex py={10} px={4} pl={{base:4, md:10}} maxW={"full"} mx={"auto"} flexDirection={"column"}>
+        <ProfileHeader/>
+      </Flex>
+      {/* Flex container for the profile tabs & posts */}
+      <Flex px={{base:2, sm:4}} maxW={"ful"} mx={"auto"} borderTop={"1px solid"} borderColor={"whiteAlpha.300"} direction={"column"}>
+        <ProfileTabs/>
+        <ProfilePosts/>
+      </Flex>
+    </Container>
   )
 }
 
