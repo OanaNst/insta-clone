fb1e2393ebf8e9b942fcc42bfe49fc4e3e18441d
Oana Niste <oananiste@gmail.com>
2024-01-04 10:51:26 +0100

    Create suggested user structure

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,8 +1,25 @@
+import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
 
-const SuggestedUser = () => {
+const SuggestedUser = ({name, followers, avatar}) => {
+  const [isFollowed, setIsFollowed] = useState(false);
+
   return (
-    <div>SuggestedUser</div>
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+      {/* Left side child with name, followers and avatar */}
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src={avatar} name={name} size={"md"} />
+        <VStack spacing={2} alignItems={"flex-start"}>
+          <Box fontSize={12} fontWeight={"bold"}>{name}</Box>
+          <Box fontSize={10} color={"gray.500"}>{followers} followers</Box>
+        </VStack>
+      </Flex>
+      {/* Right side child with follow button */}
+      <Button fontSize={12} bg={"transparent"} p={0} h={"max-content"} fontWeight={"medium"} color={"blue.400"} cursor={"pointer"} _hover={{color:"white"}} onClick={() => setIsFollowed(!isFollowed)}>
+        {isFollowed ? "Unfollow" : "Follow"}
+      </Button>
+    </Flex>
   )
 }
 
