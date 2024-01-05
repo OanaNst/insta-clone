8c0a314cabba2c892a8e9315c4e447904888428f
Oana Niste <oananiste@gmail.com>
2024-01-05 12:11:41 +0100

    Create and style tab structure

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,7 +1,38 @@
+import { Box, Flex, Text } from "@chakra-ui/react"
+import {BsBookmark, BsGrid3X3, BsSuitHeart} from "react-icons/bs";
 
 const ProfileTabs = () => {
   return (
-    <div>ProfileTabs</div>
+    <Flex w={"full"} justifyContent={"center"} gap={{base:4, sm:10}} textTransform={"uppercase"} fontWeight={"bold"}>
+
+      <Flex borderTop={"1px solid white"} alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsGrid3X3/>
+        </Box>
+        <Text fontSize={12} display={{base:"none",sm:"block"}} fontWeight={"bolder"}>
+          Posts
+        </Text>
+      </Flex>
+
+      <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsBookmark/>
+        </Box>
+        <Text fontSize={12} display={{base:"none",sm:"block"}} fontWeight={"bolder"}>
+          Saved
+        </Text>
+      </Flex>
+
+      <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsSuitHeart />
+        </Box>
+        <Text fontSize={12} display={{base:"none",sm:"block"}} fontWeight={"bolder"}>
+          Likes
+        </Text>
+      </Flex>
+
+    </Flex>
   )
 }
 
