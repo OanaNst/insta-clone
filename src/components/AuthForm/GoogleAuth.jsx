fe1f4ad6ec86ff851d202935c1392585a7f6b0df
Oana Niste <oananiste@gmail.com>
2024-01-30 15:48:34 +0100

    Move Google auth infrastructure to its own component

--- src/components/AuthForm/GoogleAuth.jsx
+++ src/components/AuthForm/GoogleAuth.jsx
@@ -1,8 +1,14 @@
+import { Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const GoogleAuth = () => {
   return (
-    <div>GoogleAuth</div>
+    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+      <Image src="/google.png" w={5} alt="Google logo"/>
+      <Text mx={"2"} color={"blue.500"}>
+        Log in with Google
+      </Text>
+  </Flex>
   )
 }
 
