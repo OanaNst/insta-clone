1195d0a48fadece0e80f1f35b29cbe1b3046a63e
Oana Niste <oananiste@gmail.com>
2024-02-02 14:11:05 +0100

    Logout user with zustand

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -2,15 +2,17 @@ import { auth } from "../firebase/firebase";
 import React from "react";
 import { useSignOut } from "react-firebase-hooks/auth";
 import useShowToast from "./useShowToast";
+import useAuthStore from "../store/authStore";
 
 const useLogout = () => {
   const [signOut, isLogginOut, error] = useSignOut(auth);
   const showToast = useShowToast();
+  const logoutUser = useAuthStore((state) => state.logout);
   const handleLogout = async () => {
     try {
       await signOut();
       localStorage.removeItem("user-info");
-      console.log("Logged out");
+      logoutUser();
     } catch (error) {
       showToast("Error", error.message, "error");
     }
