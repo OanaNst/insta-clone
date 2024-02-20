4dc6210455d2a25745f35a90a7be0698e13150f2
Oana Niste <oananiste@gmail.com>
2024-02-20 11:23:42 +0100

    Replace with sidebar items component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,38 +1,13 @@
-import { Avatar, Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
-import {Link as RouterLink} from 'react-router-dom';
-import { InstagramLogo, InstagramMobileLogo, CreatePostLogo, NotificationsLogo, SearchLogo } from "../../assets/constants";
-import {AiFillHome} from "react-icons/ai";
+import { Box, Button, Flex, Link, Tooltip } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { InstagramLogo, InstagramMobileLogo } from "../../assets/constants";
 import { BiLogOut } from "react-icons/bi";
 import React from "react";
 import useLogout from "../../hooks/useLogout";
-
+import SidebarItems from "./SidebarItems";
 
 const Sidebar = () => {
-  const sidebarItems = [
-    {
-      icon: <AiFillHome size={25}/>,
-      text: "Home",
-      link: "/",
-    },
-    {
-      icon: <SearchLogo/>,
-      text: "Search",
-    },
-    {
-      icon: <NotificationsLogo/>,
-      text: "Notifications",
-    },
-    {
-      icon: <CreatePostLogo/>,
-      text: "Create",
-    },
-    {
-      icon: <Avatar size={"sm"} name="Oana Niste" src="/profilepic.png"/>,
-      text: "Profile",
-      link: "/OanaNst"
-    },
-  ];
-  const {handleLogout, isLogginOut} = useLogout();
+  const { handleLogout, isLogginOut } = useLogout();
 
   return (
     <>
@@ -44,76 +19,68 @@ const Sidebar = () => {
         position={"sticky"}
         top={0}
         left={0}
-        px={{base:2, md:4}}
+        px={{ base: 2, md: 4 }}
       >
         <Flex direction={"column"} gap={10} w={"full"} height={"full"}>
-          <Link top={"/"} as={RouterLink} pl={2} display={{base:"none", md:"block"}} cursor={"pointer"}>
-            <InstagramLogo/>
+          <Link
+            top={"/"}
+            as={RouterLink}
+            pl={2}
+            display={{ base: "none", md: "block" }}
+            cursor={"pointer"}
+          >
+            <InstagramLogo />
           </Link>
-          <Link top={"/"} as={RouterLink} p={2} display={{base:"block", md:"none"}} borderRadius={6} _hover={{bg:"whiteAlpha.200"}} w={10} cursor={"pointer"}>
-            <InstagramMobileLogo/>
+          <Link
+            top={"/"}
+            as={RouterLink}
+            p={2}
+            display={{ base: "block", md: "none" }}
+            borderRadius={6}
+            _hover={{ bg: "whiteAlpha.200" }}
+            w={10}
+            cursor={"pointer"}
+          >
+            <InstagramMobileLogo />
           </Link>
           <Flex direction={"column"} gap={5} cursor={"pointer"}>
-            {sidebarItems.map((item, index) => (
-              <Tooltip
-                key={index}
-                hasArrow
-                label={item.text}
-                placement="right"
-                ml={1}
-                openDelay={500}
-                display={{base:"block", md:"none"}}
-              >
-                <Link
-                  display={"flex"}
-                  top={item.link || null}
-                  as={RouterLink}
-                  alignItems={"center"}
-                  gap={4}
-                  _hover={{bg:"whiteAlpha.400"}}
-                  borderRadius={6}
-                  p={2}
-                  w={{base:10, md:"full"}}
-                  justifyContent={{base:"center", md:"flex-start"}}
-                >
-                  {item.icon}
-                  <Box display={{base:"none", md:"block"}}>
-                    {item.text}
-                  </Box>
-                </Link>
-              </Tooltip>
-            ))}
+            <SidebarItems />
           </Flex>
           {/* LOGOUT */}
           <Tooltip
-                hasArrow
-                label={"Logout"}
-                placement="right"
-                ml={1}
-                openDelay={500}
-                display={{base:"block", md:"none"}}
+            hasArrow
+            label={"Logout"}
+            placement="right"
+            ml={1}
+            openDelay={500}
+            display={{ base: "block", md: "none" }}
+          >
+            <Flex
+              onClick={handleLogout}
+              alignItems={"center"}
+              gap={4}
+              _hover={{ bg: "whiteAlpha.400" }}
+              borderRadius={6}
+              p={2}
+              w={{ base: 10, md: "full" }}
+              mt={"auto"}
+              justifyContent={{ base: "center", md: "flex-start" }}
+            >
+              <BiLogOut size={25} />
+              <Button
+                display={{ base: "none", md: "block" }}
+                variant={"ghost"}
+                _hover={{ bg: "transparent" }}
+                isLoading={isLogginOut}
               >
-                <Flex
-                  onClick={handleLogout}
-                  alignItems={"center"}
-                  gap={4}
-                  _hover={{bg:"whiteAlpha.400"}}
-                  borderRadius={6}
-                  p={2}
-                  w={{base:10, md:"full"}}
-                  mt={"auto"}
-                  justifyContent={{base:"center", md:"flex-start"}}
-                >
-                  <BiLogOut size={25}/>
-                  <Button display={{base:"none", md:"block"}} variant={"ghost"} _hover={{bg:"transparent"}} isLoading={isLogginOut}>
-                    Logout
-                  </Button>
-                </Flex>
-              </Tooltip>
+                Logout
+              </Button>
+            </Flex>
+          </Tooltip>
         </Flex>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
