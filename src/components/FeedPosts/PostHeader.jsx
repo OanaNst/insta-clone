201bfb4e2314e13406ed870012edce2240c49d1a
Oana Niste <oananiste@gmail.com>
2024-03-04 16:28:06 +0100

    Fetch user header data via hook

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,34 +1,70 @@
-import { Flex, Box, Avatar, Text } from "@chakra-ui/react";
+import React from "react";
+import {
+  Flex,
+  Box,
+  Avatar,
+  SkeletonCircle,
+  Skeleton,
+  Button,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import useFollowUser from "../../hooks/useFollowUser";
 
-const PostHeader = ({username, avatar}) => {
+const PostHeader = ({ post, creatorProfile }) => {
+  const { handleFollowUser, isFollowing, isUpdating } = useFollowUser(
+    post.createdBy
+  );
   return (
     <>
-      <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        w={"full"}
+        my={2}
+      >
         <Flex alignItems={"center"} gap={2}>
-          <Avatar src={avatar} alt="User profile pic" size={"sm"}/>
+          {creatorProfile ? (
+            <Link to={`/${creatorProfile.username}`}>
+              <Avatar
+                src={creatorProfile.profilePicURL}
+                alt={"User profile pic"}
+                size={"sm"}
+              />
+            </Link>
+          ) : (
+            <SkeletonCircle size="10" />
+          )}
           <Flex fontSize={12} fontWeight={"bold"} gap={2}>
-            {username}
-            <Box color={"gray.500"}>
-              1 w
-            </Box>
+            {creatorProfile ? (
+              <Link to={`/${creatorProfile.username}`}>
+                {creatorProfile.username}
+              </Link>
+            ) : (
+              <Skeleton w={"100px"} h={"10px"} />
+            )}
+            <Box color={"gray.500"}>1 w</Box>
           </Flex>
         </Flex>
         <Box cursor={"pointer"}>
-          <Text 
+          <Button
+            size={"xs"}
+            bg={"transparent"}
             fontSize={12}
             color={"blue.500"}
             fontWeight={"bold"}
             _hover={{
-              color:"white",
+              color: "white",
             }}
             transition={"0.2s ease-in-out"}
-            >
-              Unfollow
-          </Text>
+            onClick={handleFollowUser}
+            isLoading={isUpdating}
+          >
+            {isFollowing ? "Unfollow" : "Follow"}
+          </Button>
         </Box>
       </Flex>
     </>
-  )
-}
+  );
+};
 
-export default PostHeader
+export default PostHeader;
